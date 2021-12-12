a53cd2d2f91fe1c77a95829250972e058ccb9b04
lxh <lxh@qq.com>
2021-12-12 13:06:24 +0800

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
 @section('content')
-<h1>主页</h1>
+<div class="jumbotron">
+    <h1>欢迎来到树洞</h1>
+   <h2>Welcome to tree hole</h2>
+  <p class="lead">
+  </p>
+    树洞的世界，由你去创造
+  </p>
+  <p>
+    <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+  </p>
+</div>
+
+
 @stop
